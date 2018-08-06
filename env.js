02c4013fbd394fd05c69e6620b6456419bb482cd
Timothy Krechel <hello@timothy.de>
2018-08-06 18:25:06 +0200

    - remove env.js from tracking

--- env.js
+++
@@ -1,6 +0,0 @@
-const prod = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  'process.env.TILESERVER_URL': prod ? '' : 'http://localhost:8080/data/season5/{z}/{x}/{y}.png',
-  'process.env.SPRITE_URL': prod ? '' : 'http://localhost:3000/static/sprites/sprites',
-}
