6f971c713ffbdc5ce46e6e53716f22812f8ce6c3
Timothy Krechel <hello@timothy.de>
2018-08-17 14:37:13 +0200

    - fix filter function in weekSelect

--- components/challengeList.js
+++ components/challengeList.js
@@ -1,10 +1,9 @@
-import { Collapse, List } from 'antd';
-import styled from 'styled-components';
+import { Collapse, List } from "antd";
+import styled from "styled-components";
 
-import AppContext from '../utils/context';
+import AppContext from "../utils/context";
 
 const StyledCollapse = styled(Collapse)`
-
   background-color: transparent !important;
   border-bottom: none;
 
@@ -16,37 +15,31 @@ const StyledCollapse = styled(Collapse)`
   &&& .ant-collapse-content-box {
     padding: 0 !important;
   }
-`
+`;
 
 const Panel = styled(Collapse.Panel)`
-
-  border-bottom: 2px solid rgba(255,255,255,0.3 ) !important;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.3) !important;
 
   .ant-collapse-header {
-      ${props => props.isBattlePass ? (
-    'background: linear-gradient(to right, #503e2f 0%,#503e2f 50%,#3c302b 100%);'
-  ) : (
-      'background: linear-gradient(to right, #43577a 0%,#43577a 50%,#2f3e5f 100%);'
-    )}
-    height: 38px;
+    ${props =>
+      props.isBattlePass
+        ? "background: linear-gradient(to right, #503e2f 0%,#503e2f 50%,#3c302b 100%);"
+        : "background: linear-gradient(to right, #43577a 0%,#43577a 50%,#2f3e5f 100%);"} height: 38px;
     overflow: hidden;
     padding: 0 !important;
     width: 100%;
   }
 
   i {
-   display: none !important;
+    display: none !important;
   }
-
-`
+`;
 
 const ListHeadingTitle = styled.div`
-  ${props => props.isBattlePass ? (
-    'background: linear-gradient(to right, #976d3e 0%,#624a32 100%);'
-  ) : (
-      'background: linear-gradient(to right, #708db7 0%,#51688d 100%);'
-    )}
-  height: 38px;
+  ${props =>
+    props.isBattlePass
+      ? "background: linear-gradient(to right, #976d3e 0%,#624a32 100%);"
+      : "background: linear-gradient(to right, #708db7 0%,#51688d 100%);"} height: 38px;
   margin-left: -12px;
   padding: 0 16px 0 26px;
   transform: skewX(25deg);
@@ -59,10 +52,10 @@ const ListHeadingTitle = styled.div`
     font-size: 11px;
     transform: skewX(-25deg);
   }
-`
+`;
 
 const Minimize = styled.div`
-  border: 2px solid rgba(255,255,255,0.7);
+  border: 2px solid rgba(255, 255, 255, 0.7);
   width: 38px;
   height: 38px;
   position: absolute;
@@ -73,35 +66,34 @@ const Minimize = styled.div`
   }
 
   &::after {
-    content: '-';
-    display: ${props => props.active ? 'inline-block' : 'none'};
-    font-family: 'Luckiest Guy';
+    content: "-";
+    display: ${props => (props.active ? "inline-block" : "none")};
+    font-family: "Luckiest Guy";
     font-size: 38px;
     line-height: 44px;
     text-align: center;
     width: 100%;
-  
   }
   &::before {
-    content: '+';
-    display: ${props => props.active ? 'none' : 'inline-block'};
-    font-family: 'Luckiest Guy';
+    content: "+";
+    display: ${props => (props.active ? "none" : "inline-block")};
+    font-family: "Luckiest Guy";
     font-size: 38px;
     line-height: 44px;
     text-align: center;
     width: 100%;
   }
-`
+`;
 
 const ChallengeList = styled(List)`
-  margin-bottom: ${props => props.useMargin ? '4px' : '0px'};
-`
+  margin-bottom: ${props => (props.useMargin ? "4px" : "0px")};
+`;
 
 const Challenge = styled(List.Item)`
-
   &&& {
-    background-color: ${props => props.highlighted ? 'rgba(250, 219, 20, 0.75)' : 'transparent'};
-    border-top: 2px solid rgba(255,255,255,0.2);
+    background-color: ${props =>
+      props.highlighted ? "rgba(250, 219, 20, 0.75)" : "transparent"};
+    border-top: 2px solid rgba(255, 255, 255, 0.2);
     border-bottom: none;
     min-height: 40px;
     padding: 8px 16px;
@@ -115,14 +107,14 @@ const Challenge = styled(List.Item)`
     align-items: flex-end;
     width: 100%;
   }
-`
+`;
 
 const ChallengeTitle = styled.div`
-  color: ${props => props.highlighted ? '#fff' : 'rgba(255,255,255,0.7)'};
+  color: ${props => (props.highlighted ? "#fff" : "rgba(255,255,255,0.7)")};
   font-weight: 300;
   transition: color 200ms ease-in-out;
   width: 100%;
-`
+`;
 
 const ChallengeProperties = styled.div`
   background: #f00;
@@ -132,28 +124,26 @@ const ChallengeProperties = styled.div`
   font-weight: 900;
   font-size: 12px;
   line-height: 12px;
-`
+`;
 
 const ChallengeItem = ({ challenge, highlighted, setHighlighted }) => (
   <Challenge
     onMouseEnter={() => setHighlighted([challenge.id])}
     onMouseLeave={() => setHighlighted([])}
-    highlighted={highlighted ? 1 : 0}>
-    <ChallengeTitle highlighted={highlighted ? 1 : 0}>{challenge.getName()}</ChallengeTitle>
+    highlighted={highlighted ? 1 : 0}
+  >
+    <ChallengeTitle highlighted={highlighted ? 1 : 0}>
+      {challenge.getName()}
+    </ChallengeTitle>
   </Challenge>
-)
+);
 
 export default class Challenges extends React.Component {
-
   state = {
-    activeKeys: [
-      'weekly',
-      'battlePass'
-    ]
-  }
+    activeKeys: ["weekly", "battlePass"]
+  };
 
   toggleActive = ({ key }) => {
-
     const { activeKeys } = this.state;
 
     const index = activeKeys.indexOf(key);
@@ -165,18 +155,21 @@ export default class Challenges extends React.Component {
 
     this.setState({
       activeKeys
-    })
-
-  }
+    });
+  };
 
   render() {
     return (
       <AppContext.Consumer>
-        {(context) => (
-          <StyledCollapse bordered={false} defaultActiveKey={this.state.activeKeys} activeKey={this.state.activeKeys}>
+        {context => (
+          <StyledCollapse
+            bordered={false}
+            defaultActiveKey={this.state.activeKeys}
+            activeKey={this.state.activeKeys}
+          >
             <Minimize
-              onClick={() => this.toggleActive({ key: 'weekly' })}
-              active={this.state.activeKeys.includes('weekly')}
+              onClick={() => this.toggleActive({ key: "weekly" })}
+              active={this.state.activeKeys.includes("weekly")}
             />
             <Panel
               header={
@@ -188,19 +181,26 @@ export default class Challenges extends React.Component {
             >
               <ChallengeList
                 size="large"
-                dataSource={context.challenges.getChallenges().filter(challenge => challenge.isBattlePass)}
+                dataSource={context.challenges
+                  .getChallenges()
+                  .filter(challenge => !challenge.isBattlePass)}
                 renderItem={challenge => (
                   <ChallengeItem
-                    highlighted={context.highlightedChallengeIds.includes(challenge.id)}
+                    highlighted={context.highlightedChallengeIds.includes(
+                      challenge.id
+                    )}
                     setHighlighted={context.setHighlighted}
-                    challenge={challenge} />)}
+                    challenge={challenge}
+                  />
+                )}
               />
             </Panel>
             <Minimize
-              onClick={() => this.toggleActive({ key: 'battlePass' })}
-              active={this.state.activeKeys.includes('battlePass')}
+              onClick={() => this.toggleActive({ key: "battlePass" })}
+              active={this.state.activeKeys.includes("battlePass")}
             />
-            <Panel isBattlePass
+            <Panel
+              isBattlePass
               header={
                 <ListHeadingTitle isBattlePass>
                   <p>Battle Pass Challenges</p>
@@ -210,17 +210,23 @@ export default class Challenges extends React.Component {
             >
               <ChallengeList
                 size="large"
-                dataSource={context.challenges.getChallenges().filter(challenge => !challenge.isBattlePass)}
+                dataSource={context.challenges
+                  .getChallenges()
+                  .filter(challenge => challenge.isBattlePass)}
                 renderItem={challenge => (
                   <ChallengeItem
-                    highlighted={context.highlightedChallengeIds.includes(challenge.id)}
+                    highlighted={context.highlightedChallengeIds.includes(
+                      challenge.id
+                    )}
                     setHighlighted={context.setHighlighted}
-                    challenge={challenge} />)}
+                    challenge={challenge}
+                  />
+                )}
               />
             </Panel>
           </StyledCollapse>
         )}
       </AppContext.Consumer>
-    )
+    );
   }
 }
--- components/weekSelect.js
+++ components/weekSelect.js
@@ -1,19 +1,19 @@
-import { Select } from 'antd';
-import styled from 'styled-components';
+import { Select } from "antd";
+import styled from "styled-components";
 
-import AppContext from '../utils/context';
+import AppContext from "../utils/context";
 
 const Option = Select.Option;
 
 const Container = styled.div`
   width: 100%;
-`
+`;
 
 const StyledSelect = styled(Select)`
   width: 100%;
-`
+`;
 
-export default (props) => (
+export default props => (
   <Container>
     {/* <StyledSelect size='large' defaultValue={5} onChange={props.selectSeason}>
       <Option value={1} disabled>Season 1</Option>
@@ -23,20 +23,32 @@ export default (props) => (
       <Option value={5}>Season 5</Option>
     </StyledSelect> */}
     <AppContext.Consumer>
-      {(context) => (
-        <StyledSelect size='large' defaultValue={context.week} onChange={context.selectWeek}>
+      {context => (
+        <StyledSelect
+          size="large"
+          defaultValue={context.week}
+          onChange={context.selectWeek}
+        >
           <Option value={1}>Week 1</Option>
           <Option value={2}>Week 2</Option>
           <Option value={3}>Week 3</Option>
           <Option value={4}>Week 4</Option>
-          <Option value={5} disabled>Week 5</Option>
-          <Option value={6} disabled>Week 6</Option>
-          <Option value={7} disabled>Week 7</Option>
-          <Option value={8} disabled>Week 8</Option>
-          <Option value={9} disabled>Week 9</Option>
-          <Option value={10} disabled>Week 10</Option>
+          <Option value={5}>Week 5</Option>
+          <Option value={6}>Week 6</Option>
+          <Option value={7} disabled>
+            Week 7
+          </Option>
+          <Option value={8} disabled>
+            Week 8
+          </Option>
+          <Option value={9} disabled>
+            Week 9
+          </Option>
+          <Option value={10} disabled>
+            Week 10
+          </Option>
         </StyledSelect>
       )}
     </AppContext.Consumer>
   </Container>
-)
+);
